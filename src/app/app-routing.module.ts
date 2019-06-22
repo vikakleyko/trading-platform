6b21e76c28aa1c5a94aeb249cc46d1bf1ccf5497
vikakleyko <viktoria.kleyko@gmail.com>
2019-06-22 14:21:19 +0200

    changes:
    - Login and Platform components added, app module and routing module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {PlatformComponent} from './platform/platform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+  { path: 'platform', component: PlatformComponent},
+  { path: 'login', component: LoginComponent },
 
+  { path: '**', redirectTo: '' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
